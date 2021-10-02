7f147c1a8ef33a44eede7c55c6828b0cda70cb9c
Abeer <abeeraftab01@gmail.com>
2021-10-03 00:45:15 +0530

    Cyclic Rotate

---
+++ dsa-roadmaps/Love Babbar Questions/Arrays/Cyclically rotate an array by one/solution.cpp
@@ -0,0 +1,37 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+void rotate(int arr[], int n);
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int n;
+        scanf("%d",&n);
+        int a[n] , i;
+        for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        rotate(a, n);
+        for (i = 0; i < n; i++)
+            printf("%d ", a[i]);
+        printf("\n");
+    }
+        return 0;
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+void rotate(int arr[], int n)
+{
+    for(int i = 0; i<n; i++)
+    {
+        swap(arr[i], arr[n-1]);
+    }
+}
