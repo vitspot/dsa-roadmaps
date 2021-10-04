617f585ee54ec7819dace2b95f21707e7fbb28f4
Arnab Dutta <arnabduttasocial@gmail.com>
2021-10-04 10:12:15 +0530

    Create solution.cpp

---
+++ dsa-roadmaps/LeetCode Study Plan/Algorithms/ALgorithm-1/Day 3 - Two Pointers/283. Move Zeros/solution.cpp
@@ -0,0 +1,16 @@
+//O(n) Time Complexity
+//O(1) Space Complexity
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        for (;j < nums.size(); j++) {
+            nums[j] = 0;
+        }
+    }
+};
