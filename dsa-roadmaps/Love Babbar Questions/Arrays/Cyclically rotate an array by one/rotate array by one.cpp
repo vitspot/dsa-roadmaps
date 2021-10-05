83e0a954babd7245a9282be0f42d1c96ec4bfadc
Pranjal Gupta <66200797+P1210@users.noreply.github.com>
2021-10-05 10:19:41 +0530

    Adding Kadane's Algorithm.cpp , Cycle rotate array by one.cpp and reverse string.cpp (#32)
    * Add files via upload
    
    * Update Kadane’s Algorithm.cpp
    
    * Cycle Array by one
    
    * Update Kadane’s Algorithm.cpp
    
    * Update Kadane’s Algorithm.cpp
    
    * Update rotate array by one.cpp
    
    * Update Kadane’s Algorithm.cpp
    
    * adding reverse string program

---
+++ dsa-roadmaps/Love Babbar Questions/Arrays/Cyclically rotate an array by one/rotate array by one.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+     int n;
+    cout << "Enter limit: ";
+    cin>>n;
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+     
+    int m = sizeof(a) / sizeof(a[0]);
+
+    int x = a[m - 1], i;
+    for (i = m - 1; i > 0; i--)
+    a[i] = a[i - 1];
+    a[0] = x;
+
+    for (i = 0; i < n; i++){
+        cout << a[i];
+    }
+
+    return 0;
+}
