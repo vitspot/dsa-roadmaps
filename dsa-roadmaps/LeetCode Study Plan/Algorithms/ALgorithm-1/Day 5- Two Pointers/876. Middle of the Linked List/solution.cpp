473a35f8f31d0a0c33785b764577df3c02e38030
Arnab Dutta <arnabduttasocial@gmail.com>
2021-10-05 10:33:03 +0530

    Create solution.cpp

---
+++ dsa-roadmaps/LeetCode Study Plan/Algorithms/ALgorithm-1/Day 5- Two Pointers/876. Middle of the Linked List/solution.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+     ListNode* middleNode(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next)
+            slow = slow->next, fast = fast->next->next;
+        return slow;
+    }
+};
