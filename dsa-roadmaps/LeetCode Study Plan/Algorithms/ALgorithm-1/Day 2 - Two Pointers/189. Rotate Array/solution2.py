5003c1366cf11301b4bf77fbb040351112a3e8bb
Sidheshwar S <84003394+sidheshwar-s@users.noreply.github.com>
2021-10-08 00:06:51 +0530

    Leet Code Study plan (#48)

---
+++ dsa-roadmaps/LeetCode Study Plan/Algorithms/ALgorithm-1/Day 2 - Two Pointers/189. Rotate Array/solution2.py
@@ -0,0 +1,5 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        k %= len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
+        
---
+++ dsa-roadmaps/LeetCode Study Plan/Data Structure/Data Structure- 1/Merge Sorted Array/solution2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        del nums1[m:]
+        nums1 += nums2
+        nums1.sort()
+            
+        
